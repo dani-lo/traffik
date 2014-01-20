e570e2b9539db9998f7465ad91744ddb1134771c
Dani Longo <resina00@gmail.com>
2014-01-20 14:02:31 +0700

    small change for css build/minify

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
                     cleancss: true
                 },
                 files: {
-                    "/app/css/app.css": "/app/css/app.less"
+                    "app/css/app.css": "app/css/app.less"
                 }
             }
         },
